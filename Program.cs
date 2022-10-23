f621f4f0dc5a5dc134f946dd88db456fdf53f0b5
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-10-23 16:47:50 +0300

    not finished

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Input an integer number");
+Convert.ToInt32(Console.ReadLine());
+int [] array = {0};
+int Length = array.Length;
+int index = 0; 
+Console.WriteLine(array[index] = 2);
